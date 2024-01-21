0435636e3bce745fc6d992d6d8e4b3bba86910d9
anumeetkumar <anumeetkumar@gmail.com>
2024-01-21 14:05:58 +0530

    added user schema

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const express = require("express");
 const app = express();
+const { prisma } = require("./db");
 
-app.post("/", async (req, res) => {
+app.get("/", async (req, res) => {
   console.info("login");
   res.status(200).json({ status: true, msg: "Server running" });
 });
 
 app.post("/login", async (req, res) => {
+  const user = await prisma.users.findFirst({
+    where: { email: req.body.email },
+  });
   console.info("login");
   res.status(200).json({ status: true, msg: "Server running login" });
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, phone, password } = req.body;
+  if (!name || !email || !phone || !work || !password) {
+    res.status(422).json("fill all the fields");
+  }
+  try {
+    const user = await prisma.users.findFirst({
+      where: { email: email },
+    });
+    if (user) {
+      res.status(400).json({ status: true, message: "Already register" });
+    } else {
+      const newUser = await prisma.users.create({
+        data: {
+          name,
+          email,
+          phone,
+          password,
+        },
+      });
+    }
+  } catch (err) {}
 });
 
 app.listen(5000, () => {
