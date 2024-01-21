6f5979967a38c7311ae980c2a08ab098248509c9
anumeetkumar <anumeetkumar@gmail.com>
2024-01-21 12:02:59 +0530

    login

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.post("/", async (req, res) => {
+  console.info("login");
+  res.status(200).json({ status: true, msg: "Server running" });
+});
+
 app.post("/login", async (req, res) => {
   console.info("login");
-  res.send("login");
+  res.status(200).json({ status: true, msg: "Server running login" });
 });
 
 app.listen(5000, () => {
