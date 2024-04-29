4cebaebc50cc58cb41e0253e7146970c643e707c
anumeetkumar <anumeetkumar@gmail.com>
2024-04-29 09:31:47 +0530

    feat: added todo list

--- index.js
+++ index.js
@@ -38,6 +38,40 @@ app.post("/login", async (req, res) => {
   }
 });
 
+app.post("/addProduct", async (req, res) => {
+  const { name, images, availability, price, description } = req.body;
+  if (!name || !images || !availability || !price || !description) {
+    res.status(422).json("fill all the fields");
+  }
+  try {
+    const newProduct = await prisma.products.create({
+      data: {
+        name,
+        images,
+        availability,
+        price,
+        description,
+      },
+    });
+    console.log("newProduct", newProduct);
+    res
+      .status(201)
+      .json({ status: true, message: "Product Created successfully" });
+  } catch (err) {
+    res.status(400).json({ msg: err });
+  }
+});
+
+app.get("/getProducts", async (req, res) => {
+  try {
+    const allProducts = await prisma.products.findMany();
+    console.log("allProducts", allProducts);
+    res.status(201).json({ status: true, data: allProducts });
+  } catch (err) {
+    res.status(400).json({ msg: err });
+  }
+});
+
 app.post("/register", async (req, res) => {
   const { name, email, phone, password } = req.body;
   if (!name || !email || !phone || !password) {
@@ -64,6 +98,51 @@ app.post("/register", async (req, res) => {
     }
   } catch (err) {
     console.info("err", err);
+  }
+});
+
+app.post("/add-todo", async (req, res) => {
+  const { user_id, title } = req.body;
+  console.log("req.body", req.body);
+  if (!user_id || !title) {
+    return res.status(422).json("fill all the fields111");
+  }
+  try {
+    const newTodo = await prisma.todo.create({
+      data: {
+        user_id,
+        title,
+      },
+    });
+    res
+      .status(201)
+      .json({ status: true, message: "Task created successfully" });
+  } catch (err) {
+    console.info("err", err);
+    res.status(400).json({ status: true, message: "Something went wrnong" });
+  }
+});
+
+app.get("/todo", async (req, res) => {
+  const { user_id } = req.body;
+  console.log("req.body", req.body);
+  if (!user_id) {
+    return res.status(422).json("user id is required");
+  }
+  try {
+    const userTasks = await prisma.todo.findMany({
+      where: {
+        user_id: user_id,
+      },
+    });
+    res.status(201).json({
+      status: true,
+      message: "Success",
+      data: userTasks,
+    });
+  } catch (err) {
+    console.info("err", err);
+    res.status(400).json({ status: true, message: "Something went wrnong" });
   }
 });
 
